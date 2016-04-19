511c288eb56e55d5ca00056b1356c8559e84c3ff
gaupoit <gaupoit@outlook.com>
2016-04-19 23:12:07 +0800

    Refactor, add check all function

--- html/wp-content/plugins/prevent-direct-access/js/custom-file.js
+++ html/wp-content/plugins/prevent-direct-access/js/custom-file.js
@@ -1,32 +1,40 @@
-(function() {
+(function(window, $) {
 	var customFile = {
-		preventFile : _preventFile,
-		copyToClipboard: _copyToClipboard
+		preventFile : _pda_preventFile,
+		copyToClipboard: _pda_copyToClipboard,
+		pda_prevent_all: _pda_prevent_all
 	};
-	function _preventFile(fileId){
-		var checkBoxId = "#ckb_" + fileId;
-		var isPrevented = jQuery(checkBoxId).is(':checked') ? 1 : 0;
 
-		jQuery.ajax({
+	function _pda_prevent_all() {
+		$('.pda_cbk').each(function() {
+			$(this).trigger("click");
+		});
+	}
+
+	function _pda_preventFile(fileId){
+		var checkBoxId = "#ckb_" + fileId;
+		var isPrevented = $(checkBoxId).is(':checked') ? 1 : 0;
+
+		$.ajax({
 		    url: ajax_object.ajaxurl, // this is the object instantiated in wp_localize_script function
 		    type: 'POST',
 		    data:{
 		      action: 'myaction',
 		      id: fileId, // this is the function in your functions.php that will be triggered
 		      is_prevented: isPrevented,
-					security_check: jQuery(checkBoxId).attr('nonce')
+					security_check: $(checkBoxId).attr('nonce')
 		    },
 		    success: function( data ){
 		      //Do something with the result from server
 		      if(typeof data.error !== 'undefined') {
-		      	jQuery(checkBoxId).prop('checked', false);
+		      	$(checkBoxId).prop('checked', false);
 		      	alert(data.error);
 		      } else if (data == 'invalid_nonce') {
 		      	alert('No! No! No! Verify Nonce Fails!');
-		      	if (jQuery(checkBoxId).is(':checked')) {
-		      		jQuery(checkBoxId).prop('checked', false);
+		      	if ($(checkBoxId).is(':checked')) {
+		      		$(checkBoxId).prop('checked', false);
 		      	} else {
-		      		jQuery(checkBoxId).prop('checked', true);
+		      		$(checkBoxId).prop('checked', true);
 		      	}
 		      } else {
 		      	var labelId = "#custom_url_" + data.post_id;
@@ -34,10 +42,10 @@
 		      	var divCustomUrlId = '#custom_url_div_' + data.post_id;
 		      	var custom_url_class = '.custom_url_' + data.post_id;
 		      	if(data.is_prevented === "1"){
-		      		jQuery(custom_url_class).fadeIn();
-		      		jQuery(labelId).val(data.url);
+		      		$(custom_url_class).fadeIn();
+		      		$(labelId).val(data.url);
 		      	} else {
-		      		jQuery(custom_url_class).fadeOut();
+		      		$(custom_url_class).fadeOut();
 		      	}
 		      }
 		    },
@@ -50,17 +58,17 @@
 
 	window.customFile = customFile;
 
-	function _copyToClipboard(btn, txt_input) {
-  		var $temp = jQuery("<input>");
-  		jQuery("body").append($temp);
-  		$temp.val(jQuery(txt_input).val()).select();
+	function _pda_copyToClipboard(btn, txt_input) {
+  		var $temp = $("<input>");
+  		$("body").append($temp);
+  		$temp.val($(txt_input).val()).select();
   		document.execCommand("copy");
   		$temp.remove();
 
-  		jQuery(btn).text("URL Copied");
+  		$(btn).text("URL Copied");
 		setTimeout(function() {
-		 	jQuery(btn).text("Copy URL");
+		 	$(btn).text("Copy URL");
 		}, 5000);
 	}
 
-})(window);
+})(window, jQuery);
